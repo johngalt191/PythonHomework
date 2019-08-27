6c5e0a79090230c8c738dddf05e41b77044d50d0
John <48985765+johngalt191@users.noreply.github.com>
2019-08-27 14:09:07 +0300

    changed setup.py

---
+++ final_task/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pycalc',  # Required
+    version='1.0',  # Required
+    description='Pure-python command-line calculator.',  # Optional
+    url='https://github.com/johngalt191/PythonHomework',  # Optional
+    author='ALIAKSANDR REPIKAU',  		# Optional
+    author_email='adgomel@gmail.com',  	# Optional
+    py_modules=["pycalc"],			# Required
+    entry_points={'console_scripts': ['pycalc=pycalc:main', ], },
+    # packages=['pycalc_project']		# Required
+    # python_requires='>3.0, <3.7'
